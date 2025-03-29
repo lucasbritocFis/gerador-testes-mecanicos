cd96383788a2efe49cc7356bd0f8340df7aedb9c
lucasbritocFis <lucasdbcavalcanti@gmail.com>
2025-03-29 20:12:42 -0300

    Update testesmecanicos.py

--- testesmecanicos.py
+++ testesmecanicos.py
@@ -18,7 +18,7 @@ st.markdown("""
 }
 
 .custom-number-input button {
-    width: 24px !important;
+    width: 12 !important;
     height: 24px !important;
     padding: 0 !important;
     margin: 0 !important;
