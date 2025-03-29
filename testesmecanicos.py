0109f0a0d9818d02a0b2ab3c2c15186119383fcf
lucasbritocFis <lucasdbcavalcanti@gmail.com>
2025-03-29 20:21:16 -0300

    Update testesmecanicos.py

--- testesmecanicos.py
+++ testesmecanicos.py
@@ -30,7 +30,7 @@ st.markdown("""
     
     /* Container dos inputs */
     .stNumberInput>div>div {
-        width: 10px !important;
+        width: 30px !important;
         background-color: transparent !important;
         border: none !important;
         padding: 0 !important;
