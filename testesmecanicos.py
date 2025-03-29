efabb5da09704f7b22b71ec58066f87bbcdd0c7d
lucasbritocFis <lucasdbcavalcanti@gmail.com>
2025-03-29 19:38:49 -0300

    Update testesmecanicos.py

--- testesmecanicos.py
+++ testesmecanicos.py
@@ -19,7 +19,7 @@ st.markdown("""
         padding: 8px 16px;
         font-size: 14px;
         font-weight: bold;
-        width: 120px !important;
+        width: 200px !important;
         transition: all 0.3s;
     }
     .stButton>button:hover {
